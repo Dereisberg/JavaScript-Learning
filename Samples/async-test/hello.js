5cd0a84e538b9616c22b98820864c0f14d06cc49
Dereisberg <49757934+Dereisberg@users.noreply.github.com>
2020-09-08 22:15:23 +0800

    add hello.js

---
+++ Samples/async-test/hello.js
@@ -0,0 +1,12 @@
+// hello.js
+console.log('init hello.js');
+const fs = require('fs');
+
+// a simple async function
+module.exports = async () =>{
+    let expression = await fs.readFile('./data.txt', 'utf-8');
+    let fn = new Function('return ' + expression);
+    let r = fn();
+    console.log('Calculate: ${expression} = ${r}');
+    return r;
+}
